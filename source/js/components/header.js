a57785dde7c05ab33964e82fc28638a45ae44685
N713 <ely137137@gmail.com>
2021-07-02 22:58:28 +0300

    Добавляет touchmove событие для header

--- source/js/components/header.js
+++ source/js/components/header.js
@@ -2,22 +2,25 @@ const header = document.body.querySelector('.header');
 const navigation = document.body.querySelector('.navigation');
 let lastScrollTop = 0;
 
-const setStickyHeader = () => {
-    document.addEventListener('scroll', () => {
-        let top = window.pageYOffset;
+const showStickyHeader = () => {
+    let top = window.pageYOffset;
 
-        if (navigation.classList.contains('none')) {
-            if (lastScrollTop > top) {
-                header.classList.add('sticky');
-                header.classList.remove('opacity');
-            } else if (lastScrollTop < top) {
-                header.classList.remove('sticky')
-                header.classList.add('opacity');
-            }
+    if (navigation.classList.contains('none')) {
+        if (lastScrollTop > top) {
+            header.classList.add('sticky');
+            header.classList.remove('opacity');
+        } else if (lastScrollTop < top) {
+            header.classList.remove('sticky')
+            header.classList.add('opacity');
         }
+    }
 
-        lastScrollTop = top;
-    });
+    lastScrollTop = top;
+}
+
+const setStickyHeader = () => {
+    document.addEventListener('scroll', showStickyHeader);
+    document.addEventListener('touchmove', showStickyHeader);
 
     if (window.matchMedia("(min-width: 1200px)").matches) {
         navigation.classList.remove('none');
